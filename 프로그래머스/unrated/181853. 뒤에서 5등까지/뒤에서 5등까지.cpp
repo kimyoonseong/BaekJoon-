44617660a11a0f4873b798fb228eae12b3120f73
kimyoonseong <37408405+kimyoonseong@users.noreply.github.com>
2023-07-15 13:56:37 +0900

    [unrated] Title: 뒤에서 5등까지, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181853. 뒤에서 5등까지/뒤에서 5등까지.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+vector<int> solution(vector<int> num_list) {
+    
+    vector<int> answer;
+    int cnt=0;
+    if(num_list.size()<=5){
+        sort(num_list.begin(),num_list.end());
+        return answer;   
+    }
+    sort(num_list.begin(),num_list.end());
+
+    for(const auto &s:num_list){
+        if(cnt<5) answer.push_back(s);
+        cnt++;
+    }
+    
+    return answer;
+}
