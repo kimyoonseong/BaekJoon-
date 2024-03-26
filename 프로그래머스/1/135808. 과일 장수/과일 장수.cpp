e74663f65549096c00aaf9f73d5b0be1a3092f49
김윤성 <37408405+kimyoonseong@users.noreply.github.com>
2024-03-26 22:13:57 +0900

    [level 1] Title: 과일 장수, Time: 0.01 ms, Memory: 4.12 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.cpp
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.cpp
@@ -1,11 +1,18 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <queue>
 using namespace std;
+priority_queue<int> pq;
 
 int solution(int k, int m, vector<int> score) {
     int answer = 0;
-    sort(score.rbegin(), score.rend());
-    for (int i=m-1; i<score.size(); i+=m) answer+= score[i]*m;
+    for(int i=0; i<score.size(); i++)
+        pq.push(score[i]);
+    while(pq.size() >= m) {
+        for(int i=0; i<m-1; i++) pq.pop();
+        answer += pq.top() * m;
+        pq.pop();
+    }
     return answer;
 }
