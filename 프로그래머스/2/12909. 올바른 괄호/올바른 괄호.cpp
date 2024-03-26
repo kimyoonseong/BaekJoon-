2771c92d38206a94c53b4e38b3c4d86ef4357501
김윤성 <37408405+kimyoonseong@users.noreply.github.com>
2024-03-26 23:45:27 +0900

    [level 2] Title: 올바른 괄호, Time: 0.66 ms, Memory: 3.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.cpp
@@ -0,0 +1,35 @@
+#include<string>
+#include <iostream>
+#include <stack>
+using namespace std;
+
+bool solution(string s)
+{
+    bool answer = true;
+    stack<char> a;
+    // if(s[0]==')') {
+    //         answer=false;
+    //         return answer;
+    //     }
+    //  a.push(s[0]);
+    for(int i=0; i<s.length(); i++){ 
+        if(a.empty() && s[i]==')') {
+            answer=false;
+            return answer;
+        }
+        if(s[i]==a.top()||a.empty()){
+            a.push(s[i]);
+        }
+        else{
+            a.push(s[i]);
+            a.pop();
+            a.pop();
+        }
+       
+        
+    }
+  
+    if(a.size()!=0) answer=false; 
+
+    return answer;
+}
