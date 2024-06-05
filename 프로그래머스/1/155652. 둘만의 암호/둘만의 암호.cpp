4722d83863218763944fb51cafa9b3f64e8c843c
김윤성 <37408405+kimyoonseong@users.noreply.github.com>
2024-06-05 18:39:27 +0900

    [level 1] Title: 둘만의 암호, Time: 0.02 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.cpp
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.cpp
@@ -8,16 +8,17 @@ string solution(string s, string skip, int index) {
      for (int i = 0; i < s.size(); i++) {
         char current = s[i];
         int count = 0;
-        while (count < index) {
-            current++;
-            if (current > 'z') {
-                current = 'a';
-            }
-            if (skip.find(current) == string::npos) {// skip에 문자가 없으면
-                count++;
-            }
-        }
-        answer += current;
+       while(count<index){
+           current++;
+           if(current>'z'){
+               current='a';
+           }
+           if(skip.find(current)!=string::npos){
+               continue;
+           }
+           count++;
+       }
+        answer+=current;
     }
     return answer;
 }
