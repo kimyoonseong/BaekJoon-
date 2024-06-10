efbd1a3e9e885a6ad749d6b8067caecd7402db0f
김윤성 <37408405+kimyoonseong@users.noreply.github.com>
2024-06-10 11:34:36 +0900

    [level 2] Title: 구명보트, Time: 1.40 ms, Memory: 4.61 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+#include <bits/stdc++.h>
+using namespace std;
+
+int solution(vector<int> people, int limit) {
+    int answer = 0;
+    sort(people.begin(),people.end());
+    int start=0, end=people.size()-1;
+    while(start<=end){
+        
+        if(people[start]+people[end]<=limit){
+            start++;
+            
+        }
+        answer++;
+        end--;
+    }
+
+    return answer;
+}
