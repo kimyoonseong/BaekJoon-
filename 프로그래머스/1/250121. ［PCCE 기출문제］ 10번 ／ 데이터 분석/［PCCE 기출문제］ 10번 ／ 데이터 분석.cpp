84bf473fda2f51f4ba1340f3826831a1f1a1c831
김윤성 <37408405+kimyoonseong@users.noreply.github.com>
2024-06-12 09:26:36 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 데이터 분석, Time: 0.46 ms, Memory: 3.83 MB -BaekjoonHub

--- 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.cpp
+++ 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.cpp
@@ -1,24 +1,47 @@
+
 #include <string>
 #include <vector>
 #include <algorithm>
-#include <unordered_map>
 
 using namespace std;
 
-vector<vector<int>> solution(vector<vector<int>> data, string ext, int val_ext, string sort_by) {
-    vector<vector<int>> answer;
-    unordered_map<string, int> kind = {{"code", 0}, {"date", 1},
-                                       {"maximum", 2}, {"remain", 3}};
-    
-    for (vector<int> d : data){
-        if (d[kind[ext]] <= val_ext)// val_ext보다 짧은것들 answer삽입
-            answer.push_back(d);
+int sort_intager = 0;
+
+bool cmp(vector<int> v1, vector<int> v2)
+{
+    return v1[sort_intager] < v2[sort_intager];
+}
+
+int string_handler(string str)
+{
+    if(str == "code")
+    {
+        return 0;
     }
-    int sort_idx = kind[sort_by];// 정렬 종류에 따라 
-     
-    
-    sort(answer.begin(), answer.end(), [sort_idx](vector<int> a, vector<int> b){
-        return a[sort_idx] < b[sort_idx];
-    });
+    else if(str == "date")
+    {
+        return 1;
+    }
+    else if(str == "maximum")
+    {
+        return 2;
+    }
+    else if(str == "remain")
+    {
+        return 3;
+    }
+}
+
+vector<vector<int>> solution(vector<vector<int>> data, string ext, int val_ext, string sort_by) 
+{
+    vector<vector<int>> answer;
+    for(int i=0; i<data.size(); i++)
+    {
+        if(data[i][string_handler(ext)] <= val_ext)
+            answer.push_back(data[i]);
+    }
+    sort_intager = string_handler(sort_by);
+    sort(answer.begin(),answer.end(),cmp);
+
     return answer;
 }
