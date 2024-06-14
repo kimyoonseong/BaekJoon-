a79447e60ca6c92c85731065469b4731e192741c
김윤성 <37408405+kimyoonseong@users.noreply.github.com>
2024-06-14 13:50:48 +0900

    [level 3] Title: 여행경로, Time: 0.01 ms, Memory: 4.19 MB -BaekjoonHub

---
+++ 프로그래머스/3/43164. 여행경로/여행경로.cpp
@@ -0,0 +1,38 @@
+#include <string>
+#include <vector>
+#include <bits/stdc++.h>
+using namespace std;
+vector<string> answer;
+bool visited[100000001];
+bool check = false;
+void dfs(string airport, vector<vector<string>> tickets,int count){
+    
+    if(count == tickets.size()){// 모두 사용
+         check = true;
+    } 
+    answer.push_back(airport);
+
+    for(int i=0; i<tickets.size(); i++){
+        if(!visited[i] and tickets[i][0] == airport){//현재공항티켓 탐색
+            visited[i] = true;
+            
+            dfs(tickets[i][1], tickets,count+1);
+            
+            if(!check){//모두 다 사용할수 없을경우
+                //cout<<"호출"<<endl;
+                answer.pop_back();
+                visited[i] = false;
+            }
+        }
+    }
+}
+
+vector<string> solution(vector<vector<string>> tickets) {
+    //vector<string> answer;
+       sort(tickets.begin(), tickets.end());
+    dfs("ICN", tickets,0);
+
+    
+    //dfs("ICN", tickets);
+    return answer;
+}
