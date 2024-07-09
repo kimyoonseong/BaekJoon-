1423dab4c245562a120ffd78b3f2984ac695599d
김윤성 <37408405+kimyoonseong@users.noreply.github.com>
2024-07-09 15:30:56 +0900

    [Silver I] Title: 안전 영역, Time: 16 ms, Memory: 2440 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.cc
+++ 백준/Silver/2468. 안전 영역/안전 영역.cc
@@ -1,8 +1,11 @@
 #include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
-int a[101][101], e[101][101], visited[101][101], n, temp, ret = 1;
-int dy[4] = {-1, 0, 1, 0}, dx[4] = {0, 1, 0, -1};
+int n;
+int dy[4]={0,1,0,-1};
+int dx[4]={-1,0,1,0};
+int arr[104][104];
+int visited[101][101];
 void dfs(int y, int x,int k){
 	visited[y][x]=1;
 
@@ -10,39 +13,46 @@ void dfs(int y, int x,int k){
 		int ny= y+dy[i];
 		int nx= x+dx[i];
 		
-		if(ny<0|| ny>=n||nx<0||nx>=n) continue;
-		if(!visited[ny][nx] &&a[ny][nx]>k) {
+		if(ny<0|| ny>=n||nx<0||ny>=n) continue;
+		if(visited[ny][nx]==0 &&arr[ny][nx]>k) {
 			dfs(ny,nx,k);
 		}
 	}
 	
 }
 int main(){
-	 ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);   
 	cin>>n;
-	
+	int max=1;
 	for(int i=0; i<n; i++){
 		for(int j=0; j<n; j++){
-			cin>>a[i][j];	
+			cin>>arr[i][j];	
 		}
 	}
 	
-	for(int k=1; k<101; k++){
-		fill(&visited[0][0], &visited[0][0] + 101 * 101, 0); 
-        int cnt = 0;  
+	for(int k=1; k<=100; k++){
+		fill(&visited[0][0], &visited[0][0] + 101 *101, 0);
+		int cnt=0;
 		for(int i=0; i<n; i++){
 			for(int j=0; j<n; j++){
-				if(a[i][j]>k && !visited[i][j] ){
+				if(arr[i][j]>k && !visited[i][j] ){
+					//cout<<"k: "<<k<<"cnt"<<cnt<<endl;
 					dfs(i,j,k);
+//					for(int o=0; o<n; o++){
+//						for(int u=0; u<n; u++){
+//						cout<<visited[o][u];	
+//						}
+//					cout<<endl;
+//					
+//					}
 					cnt++;
 				}
 				
 			}
-		}	
-		ret=max(ret,cnt);
+		}
+//		cout<<cnt<<endl;
+		if(cnt>=max) max=cnt;		
+
 	}
-	cout<<ret<<endl;
+	cout<<max;
 	return 0;	
 }
