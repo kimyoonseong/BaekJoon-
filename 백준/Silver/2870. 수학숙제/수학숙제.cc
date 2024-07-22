0bd45e9b064b7f0923eb0242d3fe2af8d847e5f8
김윤성 <37408405+kimyoonseong@users.noreply.github.com>
2024-07-22 14:28:00 +0900

    [Silver IV] Title: 수학숙제, Time: 4 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Silver/2870. 수학숙제/수학숙제.cc
+++ 백준/Silver/2870. 수학숙제/수학숙제.cc
@@ -4,8 +4,8 @@ int n;
 vector<string> v; 
 string s, ret;
 void go(){
-	while(true){  
-		if(ret.size() && ret.front() == '0')ret.erase(ret.begin()); 
+	while(true){
+		if(ret.front()=='0') ret.erase(ret.begin());
 		else break;
 	}
 	if(ret.size()==0) ret="0";
@@ -19,13 +19,13 @@ bool cmp(string a, string b){
 int main () {
 	cin >> n; 
 	for(int i = 0; i < n; i++){
-		cin >> s; 
-	 	ret = "";
-		for(int j = 0; j < s.size(); j++){
-			if(s[j] < 65)ret += s[j];
-			else if(ret.size()) go(); 
+		cin>>s;
+		ret="";
+		for(int j=0; j<s.size(); j++){
+			if(s[j]<65) ret+=s[j];
+			else if(ret.size()) go();	
 		}
-		if(ret.size()) go(); 
+		if(ret.size())go();
 	}
 	sort(v.begin(), v.end(),cmp);
 	for(auto it: v) cout<<it<<endl;
