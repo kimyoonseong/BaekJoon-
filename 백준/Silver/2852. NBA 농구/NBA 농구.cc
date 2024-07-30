4ca2212c77cf20e2f05b5e773a5e4ce98ff1fb1a
김윤성 <37408405+kimyoonseong@users.noreply.github.com>
2024-07-30 15:59:25 +0900

    [Silver III] Title: NBA 농구, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/2852. NBA 농구/NBA 농구.cc
+++ 백준/Silver/2852. NBA 농구/NBA 농구.cc
@@ -5,15 +5,16 @@ int n, o, A, B, asum, bsum;
 string s, prev; 
 //string 시간형으로 변환 
 string print(int a){ 
-    string d = "00" + to_string(a / 60); 
-    string e = "00" + to_string(a % 60); 
-    return d.substr(d.size() - 2, 2) + ":" + e.substr(e.size() - 2, 2); 
+   string d="00"+to_string(a/60);
+   string e="00"+to_string(a%60);
+   return d.substr(d.size()-2,2)+":"+e.substr(e.size()-2,2);
+   
 }
 int changeToInt(string a){
-    return atoi(a.substr(0, 2).c_str()) * 60 + atoi(a.substr(3, 2).c_str());
+    return atoi(a.substr(0,2).c_str())*60+atoi(a.substr(3,2).c_str());
 }
 void go(int &sum, string s){ 
-    sum += (changeToInt(s) - changeToInt(prev)); 
+    sum+= changeToInt(s)-changeToInt(prev);
 }
 int main(){
     
