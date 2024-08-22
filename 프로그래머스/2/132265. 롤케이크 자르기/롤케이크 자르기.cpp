15af2be0e06e166eaeb78438079236a36bad7f97
김윤성 <37408405+kimyoonseong@users.noreply.github.com>
2024-08-22 17:32:15 +0900

    [level 2] Title: 롤케이크 자르기, Time: 207.55 ms, Memory: 37.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.cpp
@@ -0,0 +1,37 @@
+#include <string>
+#include <vector>
+#include <map>
+#include <iostream>
+using namespace std;
+
+int solution(vector<int> topping) {
+     int answer = 0;
+    map<int, int> a;
+    map<int, int> b;
+
+    for(int i = 0; i < topping.size(); i++)
+    {
+        b[topping[i]]++;
+    }
+
+    for(int i = 0; i < topping.size(); i++)
+    {
+        b[topping[i]]--;
+        a[topping[i]]++;
+        
+//         auto it=b.find(i);
+//         if(it!=b.end()) b.erase(it);
+        if(b[topping[i]]==0){
+            b.erase(topping[i]);
+        }
+        if(b.size()==a.size()) {
+            
+            answer++;
+            //for(pair<int, int> it: a) cout<<it.first;
+
+            //for(auto it: b) cout<<it.first;
+       
+        }
+    }
+    return answer;
+}
