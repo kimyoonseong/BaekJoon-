12d0bc243308b082cd1550f8db36a6e6ee918088
김윤성 <37408405+kimyoonseong@users.noreply.github.com>
2024-08-24 20:01:52 +0900

    [level 2] Title: 땅따먹기, Time: 12.89 ms, Memory: 31.2 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.cpp
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.cpp
@@ -7,14 +7,15 @@ int solution(vector<vector<int> > land)
     int answer = 0, size = land.size();
     vector<vector<int> > dp(size, vector<int>(4, 0));
     for (int i=0; i<4; i++) dp[0][i] = land[0][i];
+    
     for (int i=1; i<size; i++) {
         for (int j=0; j<4; j++) {
-            int prev_max = 0;
+            int prev = 0;
             for (int k=0; k<4; k++) {
-                if (k == j) continue ;
-                prev_max = max(prev_max, dp[i-1][k]);
+                if(k==j) continue;
+                prev=max(prev,dp[i-1][k]);
             }
-            dp[i][j] = prev_max + land[i][j];
+            dp[i][j]=prev+land[i][j];
         }
     }
     for (int i=0; i<4; i++) answer = max(answer, dp[size-1][i]);
