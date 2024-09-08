2506bc222f6b874e617377f28eaffec9e58c97ae
김윤성 <37408405+kimyoonseong@users.noreply.github.com>
2024-09-08 21:34:04 +0900

    [level 2] Title: 호텔 대실, Time: 0.36 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.cpp
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.cpp
@@ -13,6 +13,7 @@ int solution(vector<vector<string>> book_time)
         int end=stoi(book_time[i][1].substr(0,2))*100 + stoi(book_time[i][1].substr(3))+10;
         
         if(end % 100 >= 60) end +=40;
+      
         for(int j=s;j<end;j++) room[j]++;
     }
     for(int i=0;i<2400;i++) answer = max(answer, room[i]);
