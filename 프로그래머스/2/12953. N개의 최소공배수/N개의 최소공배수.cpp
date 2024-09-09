8644e52cd4bcbe55a81cb21e27ce20013ea88080
김윤성 <37408405+kimyoonseong@users.noreply.github.com>
2024-09-09 21:19:58 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.cpp
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.cpp
@@ -2,17 +2,16 @@
 #include <vector>
 #include <bits/stdc++.h>
 using namespace std;
-int Gcd(int a ,int b)
-{
-    int A = max(a, b);
-    int B = min(a, b);
-    while(A%B!=0){
-        int tmp= A%B;
+int Gcd(int a,int b){
+    int A= max(a,b);
+    int B= min(a,b);
+    int temp;
+    while(A%B!=0){
+        temp=A%B;
         A=B;
-        B=tmp;
-        
+        B=temp;
     }
-    return B;
+    return B;
 }
 int solution(vector<int> arr) {
     int answer = arr[0];
@@ -21,7 +20,6 @@ int solution(vector<int> arr) {
         int GCD = Gcd(answer, arr[i]);
         int LCM = answer * arr[i] / GCD;
         answer = LCM;
-        //cout<<";;";
     }
     return answer;
 }
