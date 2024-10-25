39ade147e767c93dcd1e0621a9eadfa6de738b87
김윤성 <37408405+kimyoonseong@users.noreply.github.com>
2024-10-25 13:00:11 +0900

    [level 3] Title: 헤비 유저가 소유한 장소, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
+++ 프로그래머스/3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
@@ -1,13 +1,18 @@
--- 코드를 입력하세요
-SELECT ID,NAME,HOST_ID
-FROM PLACES
-WHERE HOST_ID IN
-(SELECT  HOST_ID
-FROM PLACES
-GROUP BY HOST_ID
-HAVING COUNT(ID)>1)
-ORDER BY ID
-#SELECT  ID, COUNT(ID)
-#FROM PLACES
-#GROUP BY HOST_ID
-#HAVING COUNT(ID)>1
+# -- 코드를 입력하세요
+# SELECT ID,NAME,HOST_ID
+# FROM PLACES
+# WHERE HOST_ID IN
+# (SELECT  HOST_ID
+# FROM PLACES
+# GROUP BY HOST_ID
+# HAVING COUNT(ID)>1)
+# ORDER BY ID
+
+
+select id,name,host_id
+from places
+where host_id in (
+select host_id 
+from places
+group by host_id
+having count(id)>1)
