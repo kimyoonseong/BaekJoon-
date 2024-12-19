3ffee75039026450f128a678be1618b6839e2c30
김윤성 <37408405+kimyoonseong@users.noreply.github.com>
2024-12-19 20:49:07 +0900

    [Gold IV] Title: 이분 그래프, Time: 696 ms, Memory: 17768 KB -BaekjoonHub

---
+++ 백준/Gold/1707. 이분 그래프/이분 그래프.cc
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <vector>
+#include <string.h>
+using namespace std;
+
+#define RED 2
+#define BLUE 3
+
+vector<int> vect[20001]; 
+int visited[20001];      
+int V, E;
+
+void DFS(int start)
+{
+    //방문안한 점이면 RED
+    if (visited[start] == 0)
+        visited[start] = RED;
+
+ 
+    for (int i = 0; i < vect[start].size(); i++)
+    {
+        int idx = vect[start][i];
+        if (visited[idx] == 0) 
+        {
+            if (visited[start] == RED)
+                visited[idx] = BLUE;
+            else if (visited[start] == BLUE)
+                visited[idx] = RED;
+
+            DFS(idx);
+        }
+    }
+}
+
+
+int check()
+{
+    
+    for (int i = 1; i <= V; i++)
+    {
+        for (int j = 0; j < vect[i].size(); j++)
+        {
+            int idx = vect[i][j];
+            if (visited[i] == visited[idx]) 
+            {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+int main()
+{
+    int T;   
+    int u, v; 
+
+    cin >> T;
+    while (T--)
+    {
+        cin >> V >> E;
+        for (int i = 0; i < E; i++)
+        {
+            cin >> u >> v;
+            vect[u].push_back(v);
+            vect[v].push_back(u);
+        }
+
+        
+        //빠짐없이 방문하기 위해 1부터 원소 개수까지 다 방문해봄
+        for (int i = 1; i <= V; i++)
+        {
+            if (visited[i] == 0)
+                DFS(i);
+        }
+
+        if (check() == 0) 
+            cout << "NO\n";
+        else
+            cout << "YES\n";
+
+        memset(visited, 0, sizeof(visited));
+        memset(vect, 0, sizeof(vect));
+    }
+}
