df96ac6ad4e3ca273461b2bdfba4c33898d1d2da
김윤성 <37408405+kimyoonseong@users.noreply.github.com>
2025-08-20 16:12:47 +0900

    [level 2] Title: 프로세스, Time: 0.03 ms, Memory: 4.22 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.cpp
+++ 프로그래머스/2/42587. 프로세스/프로세스.cpp
@@ -3,28 +3,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 vector <pair<int,int>> v;
-// bool cmp(pair<int, int> a, pair<int,int> b){
-//     if(a.second==b.second){
-        
-//         return a.first<b.second;
-//     }
-//     return a.second>b.second;
-// }
+
 int solution(vector<int> priorities, int location) {
-//     int answer = 0;
-//     for(int i=0; i<priorities.size(); i++){
-//         v.push_back({i,priorities[i]});
-//     }
-//     sort(v.begin(), v.end(), cmp);
-//     for(pair<int,int> it: v){
-//         cout<<it.first<<":"<<it.second<<endl;
-//     }
-//     //int ret=v[location-1];
-//     for(int i=0; i<v.size(); i++){
-//         if(v[i].first==location){
-//             answer=i+1;
-//         }
-//     }
+
      queue<pair<int, int>> q;
     priority_queue<int> pq;
     
@@ -47,6 +28,7 @@ int solution(vector<int> priorities, int location) {
                 return answer;
             }
         } else {
+            //cout<<idx<<endl;
             q.push({idx, priority});
         }
     }
