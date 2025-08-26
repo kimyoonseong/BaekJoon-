c6189e3c8afa094ab2c5f15e4d4b963903f16112
김윤성 <37408405+kimyoonseong@users.noreply.github.com>
2025-08-26 16:01:42 +0900

    [level 3] Title: 네트워크, Time: 2.31 ms, Memory: 6.59 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.cpp
+++ 프로그래머스/3/43162. 네트워크/네트워크.cpp
@@ -3,8 +3,7 @@
 #include <iostream>
 using namespace std;
 int visited[204];
-// int dy[]={0,1,0,-1};
-// int dx[]={1,0,-1,0};
+
 void dfs(int cur, int n, vector<vector<int>> com){
     visited[cur]=1;
  
@@ -12,15 +11,17 @@ void dfs(int cur, int n, vector<vector<int>> com){
 
             if(!visited[i] && com[cur][i] == 1){
                 dfs(i, n, com);
+            }
         }
-    }
+   // cout<<cur<<endl;
 }
 int solution(int n, vector<vector<int>> computers) {
     int answer = 0;
     for(int i = 0; i < n; i++){ 
         if(!visited[i]){ 
             
-            dfs(i, n, computers); 
+            dfs(i, n, computers);
+            //cout<<" zz"<<i<<endl;
             answer++; 
         }
     }
