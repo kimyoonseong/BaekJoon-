3b443b7074479359d4fc66de7094071cec3457c3
김윤성 <37408405+kimyoonseong@users.noreply.github.com>
2025-09-06 16:13:53 +0900

    [level 1] Title: [PCCE 기출문제] 9번 / 지폐 접기, Time: 0.01 ms, Memory: 4.19 MB -BaekjoonHub

--- 프로그래머스/1/340199. ［PCCE 기출문제］ 9번 ／ 지폐 접기/［PCCE 기출문제］ 9번 ／ 지폐 접기.cpp
+++ 프로그래머스/1/340199. ［PCCE 기출문제］ 9번 ／ 지폐 접기/［PCCE 기출문제］ 9번 ／ 지폐 접기.cpp
@@ -5,9 +5,25 @@ using namespace std;
 
 int solution(vector<int> wallet, vector<int> bill) {
     int answer = 0;
-
+   
     while (true) {
-        // 조건을 만족하면 종료
+       sort(wallet.begin(), wallet.end(), greater<int>());
+        sort(bill.begin(), bill.end(), greater<int>());
+        if(wallet[0]>=bill[0] && wallet[1]>=bill[1]){
+            break;
+        }     
+        if(bill[0]>bill[1]){
+            bill[0]/=2;
+        }
+        else {
+            bill[1]/=2;
+        }
+        answer++;
+    }
+
+    return answer;
+}
+/*        // 조건을 만족하면 종료
         sort(wallet.begin(), wallet.end(), greater<int>());
         sort(bill.begin(), bill.end(), greater<int>());
         if (wallet[0] >= bill[0] && wallet[1] >= bill[1]) {
@@ -21,8 +37,4 @@ int solution(vector<int> wallet, vector<int> bill) {
             bill[1] /= 2;
         }
         
-        answer++;
-    }
-
-    return answer;
-}
+        answer++;*/
