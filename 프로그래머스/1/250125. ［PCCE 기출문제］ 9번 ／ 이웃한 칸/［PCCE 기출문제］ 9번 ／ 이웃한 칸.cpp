2895c9bd08ead6b2fe73bb8b0abb70fac0b6cd64
김윤성 <37408405+kimyoonseong@users.noreply.github.com>
2025-09-07 16:41:26 +0900

    [level 1] Title: [PCCE 기출문제] 9번 / 이웃한 칸, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/1/250125. ［PCCE 기출문제］ 9번 ／ 이웃한 칸/［PCCE 기출문제］ 9번 ／ 이웃한 칸.cpp
+++ 프로그래머스/1/250125. ［PCCE 기출문제］ 9번 ／ 이웃한 칸/［PCCE 기출문제］ 9번 ／ 이웃한 칸.cpp
@@ -7,16 +7,20 @@ using namespace std;
 int solution(vector<vector<string>> board, int h, int w) {
     int answer = 0;
     
-    int dh[4] = {0, 1, -1, 0};
-    int dw[4] = {1, 0, 0, -1};
+    int dy[4] = {0, 1, -1, 0};
+    int dx[4] = {1, 0, 0, -1};
     
     for(int i = 0; i < 4; i++){
-        if ((0 <= h + dh[i]) && (h + dh[i] < board.size()) && (0 <= w+dw[i]) && (w+dw[i] < board[0].size())){
-            if (board[h][w] == board[h+dh[i]][w+dw[i]]){
+        int ny= dx[i]+h;
+        int nx= dy[i]+w;
+        if((0<=ny) && (0<=nx) && (board.size()>ny) && (board.size()>nx)){
+            if(board[h][w]==board[ny][nx]){
                 answer++;
             }
         }
+           
     }
     
     return answer;
 }
+
