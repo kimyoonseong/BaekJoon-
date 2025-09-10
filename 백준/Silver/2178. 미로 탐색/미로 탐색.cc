d19c10e012d135764475ea964ce07311ffbf55e5
김윤성 <37408405+kimyoonseong@users.noreply.github.com>
2025-09-10 22:00:54 +0900

    [Silver I] Title: 미로 탐색, Time: 0 ms, Memory: 2104 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.cc
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.cc
@@ -1,30 +1,45 @@
-#include<bits/stdc++.h>
-using namespace std; 
-const int max_n = 104; 
-int dy[4] = {-1, 0, 1, 0};
-int dx[4] = {0, 1, 0, -1}; 
-int n, m, a[max_n][max_n], visited[max_n][max_n], y, x; 
-int main(){ 
-    scanf("%d %d", &n, &m); 
-    for(int i = 0; i < n; i++){
+#include <bits/stdc++.h>
+using namespace std;
+int dy[4]={0,-1,0,1};
+int dx[4]={1,0,-1,0};
+int visited[104][104];
+int n,m,x,y;
+int arr[104][104];
+int main(){
+	  scanf("%d %d", &n, &m); 
+
+//	for(int i=0; i<n; i++){ //개행문자 따로인식안함 
+//		for(int j=0; j<m; j++){
+//			cin>> a[i][j];
+//		}
+//	}
+	for(int i = 0; i < n; i++){
         for(int j = 0; j < m; j++){
-            scanf("%1d", &a[i][j]);
+            scanf("%1d", &arr[i][j]);
         }
     } 
-    queue<pair<int, int>> q;  
-    visited[0][0] = 1;
-    q.push({0, 0});  
-    while(q.size()){
-        tie(y, x) = q.front(); q.pop(); 
-        for(int i = 0; i < 4; i++){
-            int ny = y + dy[i]; 
-            int nx = x + dx[i]; 
-            if(ny < 0 || ny >= n || nx < 0 || nx >= m || a[ny][nx] == 0) continue; 
-            if(visited[ny][nx]) continue; 
-            visited[ny][nx] = visited[y][x] + 1; 
-            q.push({ny, nx}); 
-        } 
-    }
-    printf("%d", visited[n - 1][m - 1]); 
-    return 0;
-}  
+
+	queue<pair<int,int>> q;
+	q.push({0,0});
+	visited[0][0]=1;
+	
+	while(q.size()){
+		tie(y,x)=q.front();	
+		q.pop();
+		for(int i=0;  i<4; i++){
+			int ny=dy[i]+y;
+			int nx=dx[i]+x;
+			
+			if(ny<0||nx<0|| ny>=n||nx>=m) continue;
+			if(visited[ny][nx]|| arr[ny][nx]==0) continue;
+			
+			visited[ny][nx]=visited[y][x]+1;
+			q.push({ny,nx});
+		
+			
+		}
+	}
+		
+	cout<<visited[n-1][m-1];
+	return 0;
+}
