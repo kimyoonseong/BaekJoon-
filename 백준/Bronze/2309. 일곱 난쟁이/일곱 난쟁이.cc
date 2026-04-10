fda116caa545188ca786d1a82449bcbb86b4a3ff
김윤성 <37408405+kimyoonseong@users.noreply.github.com>
2026-04-11 08:49:16 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
@@ -1,25 +1,25 @@
 #include <bits/stdc++.h>
-#include <iostream>
-#include <vector>
-#include <algorithm>
 using namespace std;
-int a[9];
 int main(){
+    vector <int> v;
+    for(int i=0; i<9; i++){
+        int tmp;
+        cin>>tmp;
+        v.push_back(tmp);
+    }
+    sort(v.begin(),v.end());
+    do{
+        int sum=0;
+        for(int i=0; i<7; i++){
+            sum+=v[i];
+        }
+        if(sum==100){
+            for(int i=0; i<7; i++){
+                cout<<v[i]<<endl;
+            }
+            break;
+        }
 
-	
-	for(int i=0; i<9; i++){
-		cin>>a[i];	
-	}
-	sort(a,a+9);
-	do{
-		
-		int sum=0;
-		for(int i=0; i<7; i++) sum+=a[i];
-		
-		if(sum==100) break;
-		
-	}while (next_permutation(a,a+9));
-	for(int i=0; i<7; i++)cout<<a[i]<<"\n";
-	
-	return 0;	
+    }while(next_permutation(v.begin(), v.end()));
+    return 0;
 }
